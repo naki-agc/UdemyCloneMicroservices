1ed74377c77e5c15609e7fa827e417cd46ec0784
Naki <nakiagca10@gmail.com>
2023-09-16 15:28:52 +0300

    Add project files.

---
+++ Shared/FreeCourse.Shared/Dtos/Response.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class Response<T>
+    {
+        public T Data { get; private set; }
+
+        [JsonIgnore] //client tarafında gelmeyecek ama yazılımda göreceğim sadece demek JsonIgnore
+        public int StatusCode { get; private set; }
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public List<string> Errors { get; set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T>
+            {
+                Data = data,
+                StatusCode = statusCode,
+                IsSuccessful = true
+            };
+        }
+
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T>
+            {
+                Data = default(T),
+                StatusCode = statusCode,
+                IsSuccessful = true
+            };
+        }
+
+        public static Response<T> Fail(List<string> errors, int statusCode)
+        {
+            return new Response<T>
+            {
+                Errors = errors,
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+        public static Response<T> Fail(string errors, int statusCode)
+        {
+            return new Response<T>
+            {
+                Errors = new List<string>() { errors },
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+    }
+}
