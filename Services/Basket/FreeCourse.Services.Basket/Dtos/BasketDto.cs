e21a8bd88ece29ef5a26a548a0ab29f134cc73b7
Naki <nakiagca10@gmail.com>
2024-03-01 17:57:44 +0300

    BasketDto/BasketItemDto added

---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public List<BasketItemDto> basketItemDtos { get; set; }
+        public decimal TotalPrice { get => basketItemDtos.Sum(x => x.Price * x.Quantity);}
+
+    }
+}
---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketItemDto
+    {
+        public int Quantity { get; set; }
+        public string CurseId { get; set; }
+        public string CourseName{ get; set; }
+        public decimal Price { get; set; }
+    }
+}
