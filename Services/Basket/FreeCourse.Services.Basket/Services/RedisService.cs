3ab9aac8ae48820657450508e000726c03569b95
Naki <nakiagca10@gmail.com>
2024-03-01 18:24:33 +0300

    RedisService bağlantısı yapıldı.ConnectionMultiplexer sınıfı ile

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/RedisService.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public class RedisService
+    {
+        private readonly string _host;
+        private readonly int _port;
+
+        private ConnectionMultiplexer _connectionMultiplexer;  // redisle bağlantı sağlayacak sınıf 
+
+
+        public RedisService(string host, int port)
+        {
+            _host = host;
+            _port = port;
+        }
+
+
+        public void Connect() => _connectionMultiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}");
+        public IDatabase GetDb(int db = 1)=> _connectionMultiplexer.GetDatabase(db);
+
+
+    }
+
+}
