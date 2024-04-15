3e3d6d4ce626317816c606a1b93d8724047b794f
Naki <nakiagca10@gmail.com>
2024-04-15 15:06:23 +0300

    Services added

--- Services/Basket/FreeCourse.Services.Basket/Services/RedisService.cs
+++ Services/Basket/FreeCourse.Services.Basket/Services/RedisService.cs
@@ -1,4 +1,5 @@
 ﻿using StackExchange.Redis;
+using System;
 
 namespace FreeCourse.Services.Basket.Services
 {
@@ -7,7 +8,7 @@ namespace FreeCourse.Services.Basket.Services
         private readonly string _host;
         private readonly int _port;
 
-        private ConnectionMultiplexer _connectionMultiplexer;  // redisle bağlantı sağlayacak sınıf 
+        private ConnectionMultiplexer _ConnectionMultiplexer;  // redisle bağlantı sağlayacak sınıf 
 
 
         public RedisService(string host, int port)
@@ -17,8 +18,8 @@ namespace FreeCourse.Services.Basket.Services
         }
 
 
-        public void Connect() => _connectionMultiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}");
-        public IDatabase GetDb(int db = 1)=> _connectionMultiplexer.GetDatabase(db);  //redisin 1 2 3 olarak içerdiği sayılar bu sınıf içerisinde mevcutlarmıi
+        public void Connect() => _ConnectionMultiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}");
+        public IDatabase GetDb(int db = 1) => _ConnectionMultiplexer.GetDatabase(db);  //redisin 1 2 3 olarak içerdiği sayılar bu sınıf içerisinde mevcutlarmıi
 
 
     }
