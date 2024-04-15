70a6d7595a45a51fcfcd29cfcc56a6580af4d229
Naki <nakiagca10@gmail.com>
2024-04-15 15:41:50 +0300

    IDiscountService added

---
+++ Services/Discount/FreeCourse.Services.Discount/Services/IDıscountService.cs
@@ -0,0 +1,17 @@
+﻿using FreeCourse.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Services
+{
+    public interface IDıscountService
+    {
+        Task<Response<List<Models.Discount>>> GetAll();  //Models.Discount dedik çümkü namespacede sıkıntı yaratıyordu isimden 
+
+        Task<Response<Models.Discount>> GetById(int id);
+        Task<Response<NoContent>> Save(Models.Discount discount);
+        Task<Response<NoContent>> Update(Models.Discount discount);
+        Task<Response<NoContent>> Delete(int id);
+        Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userId); // bu userIdya ait bir code var mı diye bakacak ona göre veri getirecek
+    }
+}
