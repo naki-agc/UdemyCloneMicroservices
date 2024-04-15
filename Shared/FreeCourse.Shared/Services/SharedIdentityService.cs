a6f4602d6daec8037cac9ee51e689fafcebd9e61
Naki <nakiagca10@gmail.com>
2024-04-15 15:07:25 +0300

    SharedServices, ISharedIdentityService, ShareddentityService

---
+++ Shared/FreeCourse.Shared/Services/SharedIdentityService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FreeCourse.Shared.Services
+{
+    public class SharedIdentityService : ISharedIdentityService
+    {
+        private IHttpContextAccessor _httpContextAccessor;//HEM REQUESTE hem de responsa bu sııf üzerinden erişiyoruz.
+
+        public SharedIdentityService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUserId => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
