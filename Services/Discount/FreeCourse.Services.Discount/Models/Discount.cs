4f14206d02ea169c94e219099cc56c911fb5f3bd
Naki <nakiagca10@gmail.com>
2024-04-15 15:29:07 +0300

    Discount Model added

---
+++ Services/Discount/FreeCourse.Services.Discount/Models/Discount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FreeCourse.Services.Discount.Models
+{
+    [Dapper.Contrib.Extensions.Table("dicount")] // bu tablouyu postgresql içindeki oluşacak tabloya mapliyorum
+    //"discount" küçük harfle yazdık çünkü postgresqlde tablolar küçük harfle tutulur.
+    public class Discount
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
