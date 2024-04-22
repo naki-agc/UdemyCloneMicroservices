bf4e15b70300b111e4321d4bbb991e631c18a061
Naki <nakiagca10@gmail.com>
2024-04-22 17:52:07 +0300

    OrderDbContextinin eklenmesi

---
+++ Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Infrastructure
+{
+    public class OrderDbContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "ordering";
+
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Domain.OrderAggregate.Order> orders { get; set; }
+        public DbSet<Domain.OrderAggregate.OrderItem> orderItems{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().ToTable("OrderItems", DEFAULT_SCHEMA);
+
+            modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().Property(x => x.Price).HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().OwnsOne(o => o.Address).WithOwner();//ownedTypeolarak elirledik.
+            //her şey order Aggregate üzerinden gerçekleşecek.
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
