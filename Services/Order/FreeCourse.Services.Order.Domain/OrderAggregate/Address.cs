2ee7345bd584a42b394c10757b80ce13fd724c6c
Naki <nakiagca10@gmail.com>
2024-04-22 17:51:48 +0300

    Order/OrderItem/Address sınıflarının oluşturulması

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
@@ -0,0 +1,48 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Address:ValueObject //Value object çünkü idsi yok
+    {
+        //private set ettim stateleri değiştirmesin dışarıdan kimse
+        public string Province { get; private set; }
+        public string District { get; private set; }
+        public string Street { get; private set; }
+        public string ZipCode { get; private set; }
+        public string Line { get; private set; }
+
+
+        //yukarıda private dedik edemezsiniz bunun için aşağıdaki constructer kullandık
+        // aşağıda set edicez işte bu sayede
+        public Address(string province, string district, string street, string zipCode, string line)
+        {
+            Province = province;
+            District = district;
+            Street = street;
+            ZipCode = zipCode;
+            Line = line;
+        }
+
+        public void SetZipCode(string zipCode)
+        {
+            //bussines code
+            ZipCode = zipCode;
+        }
+
+
+        //yield yazarız çünkü hem return yapar hem de dizi döndürür koleksiyon döndürür
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Province;  
+            yield return District; 
+            yield return Street; 
+            yield return ZipCode;
+            yield return Line; 
+        }
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,52 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using FreeCourse.Services.Order.Domain.Core.FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Order:Entity,IAggregateRoot
+    {
+        public DateTime CreatedDate { get; private set; }
+        public Address Address{ get; private set; }
+        public string BuyerId { get; private set; }
+
+        //bu public olursa birisi data ekleyebilir olmaz ben sadece okuma yapsın istiyorum
+        private readonly List<OrderItem> _orderItems; // backing field sadece okuma yazma verir ef core fielddir bu get seti olan property yoksa fieldtır
+        
+        
+        //birbirine kapsülledim
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+
+        //order üretmek için bir constructer aşağıdadır.
+        public Order(string buyerId,Address address)
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            BuyerId = buyerId;
+        }
+
+
+
+        public void AddOrderItem(string productId,string productName, string pictureUrl, decimal price)
+        {
+            var existProduct = _orderItems.Any(x=> x.ProductId == productId);
+
+            if (!existProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+
+        //yardımcı bir field 
+        public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
+
+
+    }
+}
